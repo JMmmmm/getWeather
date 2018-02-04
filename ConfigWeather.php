767428347ce73589e66f6197db08fc13c49df1f8
Evgeniy Matjushenko <JMmmmm@mail.ru>
2018-02-04 17:39:03 +0300

    файл конфигурации

---
+++ ConfigWeather.php
@@ -0,0 +1,26 @@
+<?php
+
+const API_KEY ="fffa2e10cd7847e88f8134715180102";
+const LOCATION = "bakhchysaray";
+const NUM_OF_DAYS=2;
+const FORMAT = "json"; //изменяемая
+const FX = "no";
+const MCA = "no";
+const FX24 = "no";
+const SHOW_LOCAL_TIME = "yes";
+const LANG = "ru";
+
+
+/*
+class ConfigWeather {
+    public static $apiKey ="fffa2e10cd7847e88f8134715180102";
+    public static $LOCATION = "bakhchysaray";
+    public static $NUM_OF_DAYS=2;
+    public static $FORMAT = "json";
+    public static $FX = "no";
+    public static $MCA = "no";
+    public static $FX24 = "no";
+    public static $SHOW_LOCAL_TIME = "yes";
+    public static $LANG = "ru";
+}
+*/
